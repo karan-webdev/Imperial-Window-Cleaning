35613d99263d36006319c0ff12ee836aeed39208
karan-webdev <karan9kaushik@gmail.com>
2026-07-29 18:07:01 +1000

    Fix contact form submission and validation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero'
 import Services from './components/Services'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
+import About from './components/About'
+import FAQ from './components/FAQ'
 import ContactBanner from './components/ContactBanner'
 import Footer from './components/Footer'
 
